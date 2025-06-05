53762b04ece7e9854a5ff2d6de475199f9754dde
EFG <efg@efg.local>
2025-06-04 21:18:09 -0300

    Trabalho com dados quebrados, e regulagem 'json'.

--- distribuicao_frequencia.py
+++ distribuicao_frequencia.py
@@ -35,7 +35,13 @@ def analisar_dados_estatisticos(dados_brutos, nome_do_conjunto):
     print(f"k = {k}")
     
     # 6. Amplitude  das classes (h)
-    h = at / k
+    if at < 1:
+        h = at / k
+    else:
+        h = math.ceil(at / k)
+    
+    print ("/n6. Amplitude das classes (k): ")
+    print (f"h = {h}")
     
     classes = []
     frequencias_absolutas = []
@@ -49,9 +55,13 @@ def analisar_dados_estatisticos(dados_brutos, nome_do_conjunto):
     for i in range(k): # k = 5 (numero de classes)
         limite_superior = limite_inferior + h 
         # Classes 
-        classes.append(f"[{limite_inferior}--| {limite_superior}]")
+        classes.append(f"[{limite_inferior:.2f}--| {limite_superior:.2f}]")
         # Frequencias absolutas 
-        frequencia_absoluta = len([x for x in rol if limite_inferior <= x < limite_superior])
+        if i == k - 1:
+            frequencia_absoluta = len([x for x in rol if limite_inferior <= x <= limite_superior])
+        else:
+            frequencia_absoluta = len([x for x in rol if limite_inferior <= x < limite_superior])
+
         frequencias_absolutas.append(frequencia_absoluta)
         # Ponto medio das classes 
         pontos_medios.append((limite_inferior + limite_superior) / 2)
@@ -99,4 +109,16 @@ def analisar_dados_estatisticos(dados_brutos, nome_do_conjunto):
 #----ESTUDO DE CASO 01: idade dos alunos
 dados_idades = [21, 21, 21, 22, 22, 22, 23, 23, 23, 23, 24, 24, 25, 25, 25, 26, 27, 27, 28, 28, 30, 30, 31, 31, 31]
 df_idades = analisar_dados_estatisticos(dados_idades, "Idades dos Alunos")
-print(df_idades.to_string())
+print(df_idades.to_string(float_format="%.2f"))
+
+# ----ESTUDO DE CASO 02: Tempo de atendimento em CALL CENTER
+dados_call_center = [5.2, 8.1, 6.5, 12.0, 7.3, 5.8, 9.5, 11.2, 6.0, 7.8, 5.5, 10.1, 6.7, 7.0, 8.5, 13.5, 6.2, 7.5, 9.0, 10.5, 5.0, 8.8, 6.9, 7.2, 11.5, 6.3, 9.8, 10.0, 7.6, 8.0]
+df_call_center = analisar_dados_estatisticos(dados_call_center, "Tempo de Atendimento em Call Center")
+print(df_call_center.to_string(float_format="%.2f"))
+
+# ----ESTUDO DE CASOS 3: Idaes de Bebes de uma Padaria 
+dados_idades_bebes = [0.2, 0.9, 0.4, 0.7, 0.1, 0.8, 0.3, 0.6, 0.5, 0.2, 1.0, 0.7, 0.3, 0.9, 0.5, 0.1, 0.8, 0.4, 0.6, 1.0, 0.2, 0.7, 0.5, 0.9, 0.3]
+df_idades_bebes = analisar_dados_estatisticos(dados_idades_bebes, "Idades de Bebes de uma Padaria")
+print(df_idades_bebes.to_string(float_format="%.2f"))
+
+print(df_idades.to_json(indent = 4, orient = 'records', force_ascii = False))
