92281d9de4f20d5f19f8f5efff0fdde81ba6fdac
EFG <efg@efg.local>
2025-06-06 21:20:48 -0300

    IMPLEMENTACAO MEDIA_PONDERADA

---
+++ gerar_tabelas_homicidios.py
@@ -0,0 +1,4 @@
+import pandas as pd
+from faker import Faker as fk
+import random
+
--- medida_ponderada.py
+++ medida_ponderada.py
@@ -8,6 +8,7 @@ sensor_b_data = [20.5, 20.0, 21.0, 20.2, 20.8, 20.1, 20.6, 20.3, 20.9, 20.4]
 # Sensor C (5 leituras - Menos Preciso)
 sensor_c_data = [21.5, 19.8, 22.0, 20.0, 19.5]
 
+# todos os dados 
 all_data = sensor_a_data + sensor_b_data + sensor_c_data
 
 peso_a = [3] * len(sensor_a_data) 
@@ -27,11 +28,55 @@ for i in range(len(all_data)):
     soma_valores += (valor * peso)
     soma_dos_pesos += peso
     
-    media_sensores = soma_valores / soma_dos_pesos
-    print(f">>>Exempo 01: Dados dos Sensores<<<")
-    print(f"Soma dos valores ponderados: {soma_valores:.2f}")
-    print(f"Soma dos pesos: {soma_dos_pesos:.2f}")
-    print(f"Media ponderada das temperaturas : {media_sensores:.2f}")
+media_sensores = soma_valores / soma_dos_pesos
+print(f">>>Exempo 01: Dados dos Sensores<<<")
+print(f"Soma dos valores ponderados: {soma_valores:.2f}")
+print(f"Soma dos pesos: {soma_dos_pesos:.2f}")
+print(f"Media ponderada das temperaturas : {media_sensores:.2f}")
     
-    media_simples = sum(all_data) / len(all_data)
-    print(f"Media Aritmétrica Simples das Temperaturas: {media_simples:.2f} ºC")
+media_simples = sum(all_data) / len(all_data)
+print(f"Media Aritmétrica Simples das Temperaturas: {media_simples:.2f} ºC")
+    
+print(f"-----------------------------------------------------------------------------------------------")
+        
+# --- Exemplo 2: Média Ponderada para Experimento Online (Correção de Representatividade Desigual) ---
+
+# Dados de satisfação dos usuários (escala de 1 a 10)
+# Usuários Ativos (15 respostas)
+satisfacao_usuarios_ativos = [8, 9, 7, 8, 9, 10, 7, 8, 9, 8, 9, 7, 10, 8, 9]
+# Usuários Ocasionais (10 respostas)
+satisfacao_usuarios_ocasionais = [5, 6, 7, 4, 5, 6, 7, 4, 5, 6]
+
+amostra_completa = satisfacao_usuarios_ativos + satisfacao_usuarios_ocasionais
+
+proporcao_populacao_ativa = 0.70 # 70% de todos são ativos
+proporcao_populacao_ocasional = 0.30 # 30% de todos são ocasionais
+proporcao_ativa_amostra = len(satisfacao_usuarios_ativos)/len(amostra_completa)
+proporcao_ocasional_amostra = len(satisfacao_usuarios_ocasionais)/len(amostra_completa)
+print(f"proporção usuários ativos na amostra: {proporcao_ativa_amostra}")
+print(f"proporção usuários ocasionais na amostra: {proporcao_ocasional_amostra}")
+
+peso_ativos_balanceado = proporcao_populacao_ativa / proporcao_ativa_amostra
+peso_ocasionais_balanceado = proporcao_populacao_ocasional / proporcao_ocasional_amostra
+
+
+print(f"-----------------------------------------------------------------------------------------------")
+
+array_peso_ativos = [peso_ativos_balanceado] * len(satisfacao_usuarios_ativos)
+array_pesos_ocasionais = [peso_ocasionais_balanceado] * len(satisfacao_usuarios_ocasionais)
+array_todos_pesos= array_peso_ativos + array_pesos_ocasionais
+
+# ---CALCULOS---
+soma_valores_ponderados = 0
+soma_pesos = 0
+
+for i in range(len(amostra_completa)):
+    valor = amostra_completa[i]
+    peso = array_todos_pesos[i]
+    soma_valores_ponderados += (valor * peso)
+    soma_pesos += peso
+    
+media_satisfacao_ponderada = soma_valores_ponderados / soma_pesos
+print(f"Média Ponderada de Satisfação do Usuário: {media_satisfacao_ponderada:.2f}")
+media_simples = sum(amostra_completa) / len(amostra_completa)
+print(f"Média Simples de Satisfação do Usuário: {media_simples:.2f}")
