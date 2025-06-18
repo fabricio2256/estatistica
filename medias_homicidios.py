e7c17eefb5e953c3ed524393389df44f15275e5c
EFG <efg@efg.local>
2025-06-17 21:25:54 -0300

    Estimativa da Variabilidade do 'taxa_homicidios.csv'

--- medias_homicidios.py
+++ medias_homicidios.py
@@ -33,3 +33,80 @@ df_dados_brutos = pd.read_csv('taxa_homicidios.csv')
 print(df_dados_brutos)
 df_medias = get_media(df_dados_brutos)
 print({df_medias.to_string(float_format="%2.f")})
+
+
+
+def estimativas_variabilidade(dados_brutos, media):
+    """
+   Estimativas de Variabilidade
+   Indica o quão espalhados/dispersos os dados estão em relação ao centro (média, mediana, moda)
+      Desvios
+      Diferença entre os valores observados e uma estimativa de localização (média, mediana, moda)
+      - Desvio: tx_homicidio - media
+      - Desvio Absoluto: |tx_homicidio - media|
+      - Desvio Absoluto Médio: soma dos desvios / num desvios
+      - Variância: soma(desvio^2) / num desvios - 1
+      - Desvio Padrão: Raiz quadrada da variância
+      Estatísticas de Ordem
+      Estatísticas baseadas em dados ordenados (order)
+      - Amplitude: valor máximo - valor mínimo
+      - Percentil: Divide os valores em porcentagens           [10%, 20%, 30%, 40%, 50%, 60%, 70%, 80%, 90%, 100%]
+      - Quantil: Mesmo que percentil, mas com casas decimais   [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+      - Quartil: Divide os valores em quatro partes iguais     [Q1 = 25%, Q2 = 50%, Q3 = 75%]
+      - Amplitude Interquartil: Q3 - Q1
+      - Mediana: Divide os valores em duas partes iguais       [med = Q2]
+         Percentis
+   """
+    print(estimativas_variabilidade.__doc__)
+    # Ordenar os dados
+    dados_brutos = dados_brutos.sort_values()
+    # desvios 
+    desvios = dados_brutos - media
+    # desvios Absolutos 
+    desvios_absolutos = np.abs(desvios)
+    # desvios absolutos médios 
+    desvio_absoluto_medio = np.mean(desvios_absolutos)
+    # Variância (ddof = dlta degreee of freedom)
+    variancia = np.var(dados_brutos, ddof=1)
+    # desvio padrão 
+    desvio_padrao = np.std(dados_brutos, ddof=1)
+    # DataFrame 
+    df_desvios_individuais = pd.DataFrame({
+        'Dados Brutos': dados_brutos,
+        'Desvio (taxa homicidos - media)': desvios,
+        'Desvio Absoluto |tx_homicidio - media|': desvios_absolutos
+        })
+    df_media_desvios = pd.DataFrame({
+        'Métrica Estatística' : [
+            'Média',
+            'Desvio Padrão',
+            'variância',
+            'Desvio Absoluto Médio (DAM)'
+        ],
+        'Valor Calculado': [
+            media,
+            desvio_padrao,
+            variancia,
+            desvio_absoluto_medio
+        ]
+    })
+    # imprimir dados
+    print(df_desvios_individuais.round(2))
+    print("\n")
+    print(df_media_desvios.round(2))
+    print("\n")
+    # quantil (decimais)
+    decimais = [0.05, 0.25, 0.5, 0.75, 0.95]
+    df_quartis = pd.DataFrame(dados_brutos.quantile(decimais))
+    print(df_quartis.transpose())
+    # percentil
+    df_percentis = pd.DataFrame(dados_brutos.quantile(decimais))
+    # ESTE CODIGO:
+    # novo_indicis = []
+    # for p in decimais
+    #     novos_indicis.append(f"{p*100}%")
+    # df_percentis.index = novos_indicis
+    # RESUMISE EM:   "df_percentis.index = [f"{p*100}%" for p in decimais]"
+    df_percentis.index = [f"{p*100}%" for p in decimais]
+
+estimativas_variabilidade(df_dados_brutos['Taxa Homicídio'], np.mean(df_dados_brutos['Taxa Homicídio']))
--- percenrtis.py
+++ percenrtis.py
@@ -31,4 +31,30 @@ df_quartis = pd.DataFrame({
     'Quartil': ['Primeiro (Q1)', 'Segundo (Q2 = Mediana)', 'Terceiro (Q3)'],
     'Valor': [q1, q2, q3]
 })
+print("---Quartis das Notas---")
 print(df_quartis)
+print("\n")
+
+# --- Cáuculos das Decis
+decis_percentis = [i /10.0 for i in range(1, 11)]
+decis_valores = [df_alunos['Nota'].quantile(p) for p in decis_percentis]
+decis_nomes = [f"{i}° Decil" for i in range(1, 11)]
+print(decis_nomes)
+df_decis= pd.DataFrame({
+    'Decil': decis_nomes,
+    'Percentil': [f"{int(p*100)}%" for p in decis_percentis],
+    'Valor': decis_valores
+})
+print("---  Decis das nota s ---")
+print(df_decis)
+print("\n")
+
+amplitude_interquantil = q3 - q1
+
+df_aiq =  pd.DataFrame({
+    'Métrica': ['Amplitude Interquantil (AIQ)'],
+    'Valor': [amplitude_interquantil]
+})
+print("---Amplitude Interquantil (ATQ)---")
+print(df_aiq)
+print("\n")
